3b7dcf7c9d830ab8ec5d2130cbe19f395b323459
biantsh <shkolollibiant@gmail.com>
2024-06-11 15:09:56 +0200

    Add placeholder text in SN input field

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import '../styles/styles.css';
 
 const LoginForm = ({ newSn, onSnChange, onSubmit }) => {
+  const [placeholder, setPlaceholder] = useState('Serial number');
+
   return (
     <form onSubmit={onSubmit}>
       <div>
         <h2>Authentication</h2>
         <p>Enter your Raspberry PI's Serial Number:</p>
-        <input value={newSn} onChange={onSnChange}></input>
+        <input 
+          value={newSn} 
+          onChange={onSnChange}
+          placeholder={placeholder}
+          onFocus={() => setPlaceholder('')}
+          onBlur={(event) => {if (!event.target.value) {
+            setPlaceholder('Serial number');
+          }}}
+          required>
+        </input>
       </div>
       <div>
         <button type='submit'>Join room</button>
